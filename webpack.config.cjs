bacc3d714d8b972e89bc3e4eb2db6d36b83a8195
Matthew Holder <sixxgate@hotmail.com>
2022-07-13 11:20:58 -0500

    Fixed program execution
    - Webpack now makes the program executable.
    - Added the binary name to the binary list in the package manifest.
    - Added run configurations to build for IntelliJ.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,23 +1,29 @@
-const { resolve } = require('path')
+const { chmod, stat } = require('node:fs/promises')
+const { normalize, resolve } = require('node:path')
 const { BannerPlugin } = require('webpack')
 const WebpackBarPlugin = require('webpackbar')
 const WebpackNotifierPlugin = require('webpack-notifier')
 const nodeExternals = require('webpack-node-externals')
 
-const esmModules = [
-  'env-paths'
-]
+// These modules are ES6 modules that require import()
+const esmModules = ['env-paths']
 
-module.exports = {
+/**
+ * @param env {Object.<string, *>}
+ * @param argv {Object.<string, *>}
+ * @returns {import('webpack').Configuration}
+ */
+module.exports = (env, argv) => ({
   target: 'node14',
   entry: './src/main.ts',
   output: {
+    // $PROJECTDIR/bin/aws-credentials.cjs
     filename: 'aws-credentials.cjs',
     path: resolve(__dirname, 'bin')
   },
   module: {
     rules: [
-      // TypeScript
+      // TypeScript support
       {
         test: /\.tsx?$/u,
         use: 'ts-loader',
@@ -27,15 +33,35 @@ module.exports = {
   },
   resolve: { extensions: ['.tsx', '.ts', '.js'] },
   plugins: [
+    // Append a node shebang.
     new BannerPlugin({ banner: '#!/usr/bin/env node', raw: true, entryOnly: true }),
+    // Fancy progress bars.
     new WebpackBarPlugin({ profile: true }),
-    new WebpackNotifierPlugin()
+    // Desktop notification when done.
+    new WebpackNotifierPlugin(),
+    // Make the program executable.
+    compiler => {
+      compiler.hooks.done.tapPromise('AfterPlugin', async stats => {
+        const binPath = normalize(resolve(
+          stats.compilation.outputOptions.path,
+          stats.compilation.outputOptions.filename
+        ))
+        const binStat = await stat(binPath)
+        await chmod(binPath, (binStat.mode | 0o111) & 0o777)
+      })
+    }
   ],
+  // Only show compiled assets and build time.
   stats: { preset: 'errors-warnings', assets: true, colors: true },
+  // Make sure all node modules and npm packages are not bundled.
   externalsPresets: { node: true },
   externals: [nodeExternals({
     importType: name => esmModules.includes(name) ? `module ${name}` : `commonjs ${name}`
   })],
+  // Allow top-level await.
   experiments: { topLevelAwait: true },
-  devtool: 'inline-source-map',
-}
+  // Add source maps.
+  devtool: argv.mode === 'development' ? 'inline-source-map' : undefined,
+  // Minification
+  optimization: { minimize: argv.mode === 'production' }
+})
